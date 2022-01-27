ac801eba370ff683bd53580015ac8623f02bec8e
Tora <toradeveloping@gmail.com>
2022-01-26 18:58:50 -0600

    Add Todo list

---
+++ BlazorDemo/Model/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorDemo.Model
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
---
+++ BlazorDemo/Pages/Todo.razor.cs
@@ -0,0 +1,20 @@
+﻿using BlazorDemo.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorDemo.Pages
+{
+    public partial class Todo : ComponentBase
+    { 
+        private List<TodoItem> todos = new List<TodoItem>();
+        private string? newTodo;
+
+        private void AddToDo()
+        {
+            if(!string.IsNullOrEmpty(newTodo))
+            {
+                todos.Add(new TodoItem() { Title = newTodo });
+                newTodo = string.Empty;
+            }    
+        }
+    }
+}
